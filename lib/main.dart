0aa2193e8f9eecc8152f3bfe698eb8eecb2cc4dd
akashkumar55 <akvakv150@gmail.com>
2025-06-28 22:39:51 +0530

    Textfield and Button Added

--- lib/main.dart
+++ lib/main.dart
@@ -26,99 +26,140 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.grey[900],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  "Hive Database",
-                  style: TextStyle(
-                    color: Colors.tealAccent,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.grey[900],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
               ),
             ),
-            SizedBox(height: 150),
-            Container(margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.teal[85],
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.tealAccent.withOpacity(0.4),
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                  )
-                ]
-              ),
-              child: TextField(
+            child: Center(
+              child: Text(
+                "--:Hive Database:--",
                 style: TextStyle(
-                  color: Colors.teal[900],
-                ), // Optional: set input text color
-                decoration: InputDecoration(
-                  hintText: "Enter Product Name",
-                  prefixIcon: Icon(Icons.shopping_cart_outlined,color: Colors.teal,),
-                  hintStyle: TextStyle(color: Colors.teal.withOpacity(0.9)),
-                  filled: true, // ✅ This enables the background color
-                  fillColor: Colors.teal[85], // ✅ This is the background color
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.teal, width: 2),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.teal, width: 2),
-                  ),
+                  color: Colors.teal[50],
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Container(margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.teal[85],
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.tealAccent.withOpacity(0.4),
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                  )
-                ]
-              ),
-              child: TextField(
-                style: TextStyle(
-                  color: Colors.teal[900],
-                ), // Optional: set input text color
-                decoration: InputDecoration(
-                  hintText: "Enter Price",
-                  prefixIcon: Icon(Icons.attach_money,color: Colors.teal,),
-                  hintStyle: TextStyle(color: Colors.teal.withOpacity(0.9)),
-                  filled: true, // ✅ This enables the background color
-                  fillColor: Colors.teal[85], // ✅ This is the background color
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.teal, width: 2),
+          ),
+          SizedBox(height: 100),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.tealAccent.withOpacity(0.4),
+                  spreadRadius: 5,
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            child: TextField(
+              style: TextStyle(
+                color: const Color.fromARGB(255, 0, 80, 75),
+                fontWeight: FontWeight.bold,
+              ), // Optional: set input text color
+              decoration: InputDecoration(
+                hintText: "Enter Product Name",
+                prefixIcon: Icon(
+                  Icons.shopping_cart_outlined,
+                  color: const Color.fromARGB(255, 0, 80, 75),
+                ),
+                hintStyle: TextStyle(color: Colors.teal.withOpacity(0.9)),
+                filled: true, // ✅ This enables the background color
+                fillColor: Colors.teal[85], // ✅ This is the background color
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: const Color.fromARGB(255, 0, 80, 75),
+                    width: 2,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.teal, width: 2),
-                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.tealAccent, width: 2),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.tealAccent.withOpacity(0.4),
+                  spreadRadius: 5,
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            child: TextField(
+              style: TextStyle(
+                color: const Color.fromARGB(255, 0, 80, 75),
+                fontWeight: FontWeight.bold,
+              ), // Optional: set input text color
+              decoration: InputDecoration(
+                hintText: "Enter Price",
+                prefixIcon: Icon(
+                  Icons.attach_money,
+                  color: const Color.fromARGB(255, 0, 80, 75),
+                ),
+                hintStyle: TextStyle(color: Colors.teal.withOpacity(0.9)),
+                filled: true, // ✅ This enables the background color
+                fillColor: Colors.teal[85], // ✅ This is the background color
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: const Color.fromARGB(255, 0, 80, 75),
+                    width: 2,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.tealAccent, width: 2),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 10),
+            child: ElevatedButton(
+              onPressed: (){
+            
+              }, 
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal[900],
+                foregroundColor: Colors.teal[50],
+                elevation: 5,
+                shadowColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical:10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.save,size: 20,),
+                    SizedBox(width: 10),
+                    Text("Save",style: TextStyle(fontSize: 18),)
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
